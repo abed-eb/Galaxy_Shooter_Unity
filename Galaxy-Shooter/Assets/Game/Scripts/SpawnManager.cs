1d89d7ee3ec7be2a5838d497075ca74fa74665fb
Abdollah Ebadi <abdi.eb79@gmail.com>
2021-11-19 22:41:33 +0330

    Managed spawned

---
+++ Galaxy-Shooter/Assets/Game/Scripts/SpawnManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] powerups;
+    [SerializeField] private GameObject[] enemyShipPrefab;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemySpawnRoutine());
+        StartCoroutine(PowerupsSpawnRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    IEnumerator EnemySpawnRoutine()
+    {
+        while (true)
+        {
+            Instantiate(enemyShipPrefab[0], new Vector3(Random.Range(-7f, 7f), 7, 0), Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+    
+    IEnumerator PowerupsSpawnRoutine()
+    {
+        while (true)
+        {
+            int randomPowerup = Random.Range(0, 2);
+            Instantiate(powerups[randomPowerup], new Vector3(Random.Range(-7f, 7f),7 ,0), Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+}
